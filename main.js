689e9f407cf1c70b2f0d7da43156771e57d560fb
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-04-13 19:23:53 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-function preload()
-{}
+noseX = 0;
+noseY = 0;
+lwristX = 0;
+rwristX = 0;
+difference = 0;
 
 function setup()
 {
-    canvas = createCanvas(550, 500);
-    canvas.position(900, 200);
     video = createCapture(VIDEO);
-    video.position(50, 200);
-    pose_model = ml5.poseNet(video, modelLoaded);
-    pose_model.on("pose", gotPoses);
+    video.size(650, 500);
+    canvas = createCanvas(650, 500);
+    canvas.position(800, 180);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function modelLoaded()
@@ -16,13 +19,27 @@ function modelLoaded()
     console.log("PoseNet has been loaded succesfully");
 }
 
-function draw()
-{}
-
 function gotPoses(results)
 {
     if (results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose x + y values: x- "+noseX+" y- "+noseY);
+        lwristX = results[0].pose.leftWrist.x;
+        rwristX = results[0].pose.rightWrist.x;
+        console.log("Wrist values: leftwrist- "+lwristX+" rightwrist- "+rwristX);
+        difference = floor(lwristX - rwristX);
+        console.log(difference);
     }
+}
+
+function draw()
+{
+    background("blue")
+    document.getElementById("sq_width").innerHTML = difference + " px";
+    fill("red");
+    stroke("green")
+    square(noseX, noseY, difference);
 }
